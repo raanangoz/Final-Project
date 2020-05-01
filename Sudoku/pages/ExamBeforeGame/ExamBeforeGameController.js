2813206c1af6dfe21ad3edeafbfd9238796670c1
itzikd1 <vhhchh20>
2020-05-01 13:39:18 +0300

    exam sudoku almost done

--- Sudoku/pages/ExamBeforeGame/ExamBeforeGameController.js
+++ Sudoku/pages/ExamBeforeGame/ExamBeforeGameController.js
@@ -5,7 +5,10 @@ angular.module("sudokuApp")
         document.getElementById("prevExam").style.visibility = "hidden";
         var slideIndex = 1;
         showDivs(slideIndex);
-
+        var printedQuestions = [];
+        var questionsFromServer = [];
+        var numberOfQuestionsInDB = 6;
+        var numberOfQuestionsToAsk = 3;
         $scope.plusDivs = function (n) {
             showDivs(slideIndex += n);
         }
@@ -51,11 +54,9 @@ angular.module("sudokuApp")
 
                 //TODO raanan 10 QUESTION, 5 RANDOM
             }).then(function (response) {
-                let chosenQuestionsByID = [];
-                var numberOfQuestionsInDB = 6;
-                var numberOfQuestionsToAsk = 3;
-                var questionsFromServer = [];
-                var printedQuestions = [];
+
+
+
                 for (var i = 0; i < numberOfQuestionsInDB; i++) {
                     printedQuestions.push(false);
                 }
@@ -92,15 +93,25 @@ angular.module("sudokuApp")
 
             // let answer = document.querySelector('input[name!="null"]');
             // console.log(document.querySelector('input[name="op"]:checked').value);
-            let answer0 = $scope.c;
+            let answer0 = $scope.a;
             console.log(answer0);
-            let answer1 = $scope.a;
+            let answer1 = $scope.b;
             console.log(answer1);
-            let answer2 = $scope.b;
+            let answer2 = $scope.c;
             console.log(answer2);
+            if(answer0==questionsFromServer[0].correctAnswer&&
+                answer1==questionsFromServer[1].correctAnswer&&
+                answer2==questionsFromServer[2].correctAnswer){
+                $location.url('/pageBeforeGame');
+            }
+            else{
+                window.alert("You did not pass the test");
+            }
 
 
-            $location.url('/pageBeforeGame');
+
+
+
 
         }
 
